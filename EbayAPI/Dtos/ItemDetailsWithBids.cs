01b891816ed8e452e366d9888d16cbe18a4f9432
stratospas <89253150+stratospas@users.noreply.github.com>
2022-08-05 13:26:29 +0300

    Create ItemDetailsWithBids.cs

---
+++ EbayAPI/Dtos/ItemDetailsWithBids.cs
@@ -0,0 +1,24 @@
+using EbayAPI.Models;
+
+namespace EbayAPI.Dtos;
+
+public class ItemDetails
+{
+    
+    [Required] public int ItemId { get; set; }
+    [Required] public string Name { get; set; }
+    public decimal? BuyPrice {get; set;} = null;
+    [Required] public decimal FirstBid {get; set;}
+    public string Location { get; set; }
+    public string? Country { get; set; } = null;
+    [Required] public string Description { get; set; }
+    public DateTime? Started {get; set;} = null;
+    [Required] public DateTime Ends {get; set;}
+    public decimal? Latitude { get; set; } = null;
+    public decimal? Longitude { get; set; } = null;
+    [Required] public int SellerId { get; set; }
+    public List<ItemsCategories>? ItemCategories {get; set;} = null;
+    public List<Image>? Images {get; set;} = null;
+    public List<Bid>? Bids {get; set;} = null;
+    
+}
