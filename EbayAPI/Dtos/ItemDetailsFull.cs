10add212525c0e610d631452cc6f2e1d00103560
stratospas <89253150+stratospas@users.noreply.github.com>
2022-08-05 14:42:04 +0300

    Update and rename ItemDetailsWithBids.cs to ItemDetailsFull.cs

--- EbayAPI/Dtos/ItemDetailsFull.cs
+++ EbayAPI/Dtos/ItemDetailsFull.cs
@@ -20,5 +20,6 @@ public class ItemDetails
     public List<ItemsCategories>? ItemCategories {get; set;} = null;
     public List<Image>? Images {get; set;} = null;
     public List<Bid>? Bids {get; set;} = null;
+    public List<UserVisitedItems>? VisitedByUsers {get; set;} = null;
     
 }
