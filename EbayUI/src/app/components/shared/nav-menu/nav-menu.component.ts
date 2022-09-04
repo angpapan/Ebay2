55576eca1c85cf2cf9917dfca03f8373ebd7f4f8
Angelos Pap <sdi1800153@di.uoa.gr>
2022-09-04 09:18:14 +0300

    fix nav menu category list and search function

--- EbayUI/src/app/components/shared/nav-menu/nav-menu.component.ts
+++ EbayUI/src/app/components/shared/nav-menu/nav-menu.component.ts
@@ -3,6 +3,8 @@ import {AuthenticationService} from "../../../Services/authentication.service";
 import {Router} from "@angular/router";
 import {storageItems} from "../../../model/storageItems";
 import {MessageService} from "../../../Services/message.service";
+import {Category} from "../../../model/Category";
+import {CategoryService} from "../../../Services/category.service";
 
 @Component({
   selector: 'app-nav-menu',
@@ -16,17 +18,25 @@ export class NavMenuComponent implements OnInit {
   newMessages: number = 0;
   username: string | null = localStorage.getItem(storageItems.Username);
   selectedCategory: number = 0;
+  topCategories: Category[] = [];
+  searchText: string = "";
 
   constructor(private authSerive: AuthenticationService, private router: Router,
-              private messageService: MessageService) {}
+              private messageService: MessageService, private categoryService: CategoryService) {}
 
   ngOnInit() {
     let token: string | null = localStorage.getItem(storageItems.Token);
     this.loggedIn = token !== undefined && token !== null && token !== "";
 
+    this.categoryService.getTopCategories().subscribe({
+      next: value => this.topCategories = value
+    })
+
     if(this.role === 'user'){
       this.getNewMessages();
     }
+
+
   }
 
   getNewMessages() {
@@ -51,5 +61,10 @@ export class NavMenuComponent implements OnInit {
     this.authSerive.logout();
     this.loggedIn = false;
     this.router.navigate(['/home']);
+  }
+
+  search(): void {
+    console.log(`Text: ${this.searchText}, Category: ${this.selectedCategory}`);
+    // TODO navigate to search results page giving above parameters as state
   }
 }
