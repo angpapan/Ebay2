ee591c41e5970a58d0d9be3777fcf8ab2f051e15
Angelos Pap <sdi1800153@di.uoa.gr>
2022-09-10 13:45:47 +0300

    add free text search to item search API

--- EbayAPI/Dtos/ItemListQueryParameters.cs
+++ EbayAPI/Dtos/ItemListQueryParameters.cs
@@ -5,6 +5,7 @@ namespace EbayAPI.Dtos;
 public class ItemListQueryParameters : QueryPagingParameters
 {
     public string OrderBy { get; set; } = "price";
+    public string? Search { get; set; } = null;
     public decimal? MinPrice { get; set; } = null;
     public decimal? MaxPrice { get; set; } = null;
     public List<int>? Categories { get; set; } = null;
